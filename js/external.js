d3a58e2ef0773c4c16599250601b6ec7345388b2
Brittney Brinson <brittneyc95@outlook.com>
2022-09-09 16:12:47 -0400

    Movie prompt(correct file)

---
+++ js/external.js
@@ -0,0 +1,22 @@
+
+//Cat movies
+alert("Did you rent a movie?");
+let moviePrice = 3;
+let littleMermaid = 3 * moviePrice;
+let  howMany = prompt("What movie did you rent?")
+console.log(howMany)
+let brotherBear = 5 * moviePrice;
+let strongHercules =  moviePrice;
+
+alert("Little Mermaid:" + littleMermaid)
+alert("Brother Bear:" + brotherBear)
+alert ("Hercules:" + strongHercules)
+
+
+
+
+
+
+
+
+
