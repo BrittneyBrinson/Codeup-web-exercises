e99d1c103a7d260c30aa5850516971beb56df2ed
Brittney Brinson <brittneyc95@outlook.com>
2022-09-22 15:50:12 -0400

    Finished html intro exercise

--- warmup.js
+++ warmup.js
@@ -3,32 +3,36 @@
 
 
 
-function stringReverse (str){
-    var splitString = str.split("");
-    var reverseArray = splitString.reverse();
-    var joinArray = reverseArray.join("");
+//function stringReverse (str){
+//    var splitString = str.split("");
+//    var reverseArray = splitString.reverse();
+//    var joinArray = reverseArray.join("");
 
-    return joinArray
-};
-stringReverse();
+//    return joinArray
+//};
+//stringReverse();
 
 
-    for(let x = 1; x < 101; x++){
-        if (x % 3 === 0){
-            console.log("Fizz " + x)
-        }
-        if(x % 5 === 0){
-            console.log("Buzz " + x)
-        }
-        if (x % 5 === 0 && x % 3 === 0){
-            console.log("FizzBuzz " + x)
-        }
-         else if (x % 5 !== 0 && x % 3 !== 0){
-             console.log("No FizzBuzz for you " + x)
-        }
-};
+//    for(let x = 1; x < 101; x++){
+//        if (x % 3 === 0){
+//            console.log("Fizz " + x)
+//        }
+//        if(x % 5 === 0){
+//            console.log("Buzz " + x)
+//        }
+//        if (x % 5 === 0 && x % 3 === 0){
+//            console.log("FizzBuzz " + x)
+//        }
+//         else if (x % 5 !== 0 && x % 3 !== 0){
+//             console.log("No FizzBuzz for you " + x)
+//        }
+//};
+//Create a function to sort the array from lowest to highest. Make sure the function accepts an array and returns an array.
+function takeArray ([x]){
+    return [x.reverse]
 
-//Shuffle an array: Write a function to shuffle an array to a completely and entirely random order.//
+}
+takeArray([]);
 
 
 
