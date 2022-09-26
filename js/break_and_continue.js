0c82e0f6b6a69badf1c1b33fdf89bf84f6a7d646
Brittney Brinson <brittneyc95@outlook.com>
2022-09-26 10:25:40 -0400

    Finished warmup exercise 1.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,10 +1,15 @@
 var oddNumber = prompt("Pick an odd number between 1 and 50");
 for(oddNumber; oddNumber < 51; oddNumber++) {
     if (oddNumber % 2 === 2) {
-        prompt("The number must be ODD. Try again please.");
-
         break;
     }
-    console.log("Number to skip is: " + oddNumber)
+    prompt("The number must be ODD. Try again please.");
+    break;
+}
+
+for (oddNumber; oddNumber < 51; oddNumber++){
+    if(oddNumber === oddNumber){
+        continue;}
+    console.log("The number to skip is: " + oddNumber)
 }
 
--- js/warmup.js
+++ js/warmup.js
@@ -28,11 +28,35 @@
 //        }
 //};
 //Create a function to sort the array from lowest to highest. Make sure the function accepts an array and returns an array.
-function takeArray ([x]){
-    return [x.reverse]
+//function takeArray ([x]){
+//    return [x.reverse]
+
+//}
+//takeArray([]);
+//1.Write a function to return an array of only the Even numbers from an array with the numbers 1 through 10
+// example input: [1,2,3,4,5] expected output: [2,4]
+// using .filter() method to iterate over the given array
+// Look to see if each number doesn't have a remainder when divided by 2 (use modulo%)
+//the filter method will only return even numbers in a new array
+var randomArray = [1,2,3,4,5,6,7,8,9,10];
+const evenNumbers = randomArray.filter(number => {
+    return number % 2 === 0;
+})
+console.log(evenNumbers);
+
+
+
+
+
+
+
+
+
+
+
+// 2.Write a function that takes an array of objects and a string as arguments. Add a property with key ‘continent’ and value equal to the string to each of the objects. Return the new array of objects. Don’t mutate the original array.
+function arrayOfObjects([{x}]){
+
 
 }
-takeArray([]);
-
-
 
