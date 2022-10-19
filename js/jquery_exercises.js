475bcfd53d82194e9f2e3f2a48ef07bb0dcc2349
Brittney Brinson <brittneyc95@outlook.com>
2022-10-19 16:40:25 -0400

    Finished jQuery selectors exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,4 +2,9 @@ $(function() {
     window.onload = function () {
         alert("The DOM has loaded");
     }
+    //By adding the ".html" to the selected element (after grabbing the element by CLASS) it shows the first elements content with that CLASS name
+    // Don't forget to add the () after the ".html" method to "activate it"
+    alert($('h1').html());
+    $('*').css('color', 'hotpink');
+
 });
